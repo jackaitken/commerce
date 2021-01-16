1e0009fb714ae8e216c81dfb6a3161ad6f0ea71c
Jack Aitken <jaitk11@gmail.com>
2021-01-16 18:40:23 -0500

    small update to CreateListing form

--- commerce/auctions/forms.py
+++ commerce/auctions/forms.py
@@ -1,9 +1,16 @@
 from django.forms import ModelForm
+from django import forms
 from .models import User, Listing, Bid, Comment
 
 class CreateListing(ModelForm):
     class Meta:
         model = Listing
+
+        widgets = {
+            'description': forms.Textarea(attrs={'placeholder': 'Enter a brief description of your product',
+                                            'style':'red'})
+        }
+
         fields = [
             'title', 'description', 'price', 'image', 'category'
         ]
